8dba0fa09ae18838808a4e0f097416932db2ee44
Akash Khaitan <akashkhaitan@gmail.com>
2024-05-19 11:28:25 +0530

    feat: handled program navigation on register button

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,6 +1,17 @@
 <script setup>
+import { useRouter } from "vue-router";
+
 defineProps(["event"]);
+
+const router = useRouter();
+
+const handleRegister = () => {
+  router.push({
+    name: "event-details",
+  });
+};
 </script>
 <template>
   <p>Register for the event here</p>
+  <button @click="handleRegister">Register</button>
 </template>
