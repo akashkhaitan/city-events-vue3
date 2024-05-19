ab2fdb2fe44e519590fa20f4d674ae94a67823c5
Akash Khaitan <akashkhaitan@gmail.com>
2024-05-19 10:49:59 +0530

    feat: added pagination

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,14 +1,26 @@
 <script setup>
 import EventCard from "@/components/EventCard.vue";
 import EventService from "@/services/EventService";
-import { onMounted, ref } from "vue";
+import { computed, onMounted, ref, watchEffect } from "vue";
 
 const events = ref(null);
 
+const props = defineProps({
+  page: {
+    type: Number,
+    default: 1,
+  },
+});
+
+const page = computed(() => props.page);
+
 onMounted(() => {
-  EventService.getEvents()
-    .then((response) => (events.value = response.data))
-    .catch((error) => console.log(error));
+  watchEffect(() => {
+    events.value = null;
+    EventService.getEvents(2, page.value)
+      .then((response) => (events.value = response.data))
+      .catch((error) => console.log(error));
+  });
 });
 </script>
 
@@ -25,6 +37,21 @@ onMounted(() => {
         :key="event.id"
         :event="event"
       ></EventCard>
+
+      <div class="pagination">
+        <router-link
+          :to="{ name: 'event-list', query: { page: page - 1 } }"
+          rel="prev"
+          v-if="page != 1"
+          >Prev Page</router-link
+        >
+
+        <router-link
+          :to="{ name: 'event-list', query: { page: page + 1 } }"
+          rel="next"
+          >Next Page</router-link
+        >
+      </div>
     </div>
   </main>
 </template>
@@ -34,5 +61,23 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+
+#page-prev {
+  text-align: left;
+}
+
+#page-next {
+  text-align: right;
 }
 </style>
